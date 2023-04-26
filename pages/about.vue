12f79d355e5521c949663b1d34686259c864b8a1
gabrielmarqso <gabrielmarquesmtv123@gmail.com>
2023-04-26 17:45:49 -0300

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, magni cumque hic praesentium, quis rerum, explicabo non nam nulla quae aperiam laboriosam. Quisquam ea harum laboriosam obcaecati ducimus sit praesentium.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt deserunt ratione hic quas adipisci aliquid laboriosam neque, voluptatibus provident nam animi est, reprehenderit dicta nobis nostrum saepe tenetur facere odio!
+            
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis et atque architecto deserunt distinctio beatae iste accusamus minima quaerat mollitia? Dolorum, consequatur? Ex quia ad earum, dolores praesentium ducimus voluptatum!</p>
+    </div>
+</template>
+
+<script setup>
+    
+</script>
+
+<style scoped>
+h2{
+    margin-bottom: 10px;
+
+}
+</style>
