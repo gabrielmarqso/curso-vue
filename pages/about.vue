2599750ab64039dc4c681659d8e822a945704e85
gabrielmarqso <gabrielmarquesmtv123@gmail.com>
2023-04-27 12:38:21 -0300

    dynamic routes

--- pages/about.vue
+++ pages/about.vue
@@ -2,13 +2,15 @@
     <div>
         <h2>About</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, magni cumque hic praesentium, quis rerum, explicabo non nam nulla quae aperiam laboriosam. Quisquam ea harum laboriosam obcaecati ducimus sit praesentium.</p>
+        
+        <div>{{data}}</div>
     </div>
 </template>
 
 <script setup>
-
+    const { data } = await useFetch('/api/currency/GBP')
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
